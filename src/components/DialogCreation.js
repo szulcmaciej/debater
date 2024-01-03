0f85a72ef13f4010fa85fa28d5d384d2d2ffcab2
Maciej Szulc <szulcmaciej1@gmail.com>
2024-01-03 12:03:04 +0100

    saving dialog audio, buttons refactor

--- src/components/DialogCreation.js
+++ src/components/DialogCreation.js
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { FaTrash, FaPlay, FaRegLightbulb, FaCheck, FaRedo, FaCheckCircle, FaLightbulb } from 'react-icons/fa';
+import { FaTrash, FaPlay, FaRedo, FaLightbulb } from 'react-icons/fa';
 import { elevenTextToSpeech, concatenateStatementsAudio } from '../api/audio';
 
 const DialogCreation = ({ speakers, apiKey }) => {
     const [dialog, setDialog] = useState([]);
-  const addStatement = () => {
-    setDialog([...dialog, { speaker: speakers[0]?.voice_id, text: '', audio: null}]);
-  };
+    const [generatedDialogAudio, setGeneratedDialogAudio] = useState(null);
+    const addStatement = () => {
+        setDialog([...dialog, { speaker: speakers[0]?.voice_id, text: '', audio: null }]);
+    };
 
-  const updateStatement = (index, field, value) => {
-    const updatedDialog = [...dialog];
-    updatedDialog[index][field] = value;
-    setDialog(updatedDialog);
-  };
+    const updateStatement = (index, field, value) => {
+        const updatedDialog = [...dialog];
+        updatedDialog[index][field] = value;
+        setDialog(updatedDialog);
+    };
 
     const removeStatement = (index) => {
         const updatedDialog = [...dialog];
@@ -21,114 +22,129 @@ const DialogCreation = ({ speakers, apiKey }) => {
     };
 
 
-const generateAudioForStatement = async (index) => {
-    const statement = dialog[index];
-    const voiceId = statement.speaker; // Get the voiceId from the statement
+    const generateAudioForStatement = async (index) => {
+        const statement = dialog[index];
+        const voiceId = statement.speaker; // Get the voiceId from the statement
 
-    try {
-        const audioBlob = await elevenTextToSpeech(apiKey, voiceId, statement.text);
-        setDialog(prevDialog => prevDialog.map((line, i) => i === index ? {...line, generatedAudio: audioBlob, prevText: line.text} : line));
-    } catch (error) {
-        console.error('Error in generating audio:', error);
-    }
-};
+        try {
+            const audioBlob = await elevenTextToSpeech(apiKey, voiceId, statement.text);
+            setDialog(prevDialog => prevDialog.map((line, i) => i === index ? { ...line, generatedAudio: audioBlob, prevText: line.text } : line));
+        } catch (error) {
+            console.error('Error in generating audio:', error);
+        }
+    };
 
-const generateAudioForAllStatements = async () => {
-    try {
-        const promises = dialog
-            .filter(statement => !statement.generatedAudio) // Filter statements that don't have audio yet
-            .map((statement, index) => generateAudioForStatement(index));
-        await Promise.all(promises);
-    } catch (error) {
-        console.error('Error in generating audio:', error);
-    }
-};
+    const generateAudioForAllStatements = async () => {
+        dialog.forEach(async (statement, index) => {
+            if (!statement.generatedAudio) {
+                await generateAudioForStatement(index);
+            }
+        });
+    };
 
-const generateDialogAudio = async () => {
-    try {
-       await generateAudioForAllStatements();
-       const dialogAudio = await concatenateStatementsAudio(dialog);
-       playAudio(dialogAudio);
-    } catch (error) {
-        console.error('Error in generating audio:', error);
-    }
-};
+    const generateDialogAudio = async () => {
+        try {
+            await generateAudioForAllStatements();
+            const dialogAudio = await concatenateStatementsAudio(dialog);
+            setGeneratedDialogAudio(dialogAudio);
+        } catch (error) {
+            console.error('Error in generating audio:', error);
+        }
+    };
 
-const playStatementAudio = async (statement) => {
-    if (statement.generatedAudio) {
-        playAudio(statement.generatedAudio);
-    }
-};
+    const playStatementAudio = async (statement) => {
+        if (statement.generatedAudio) {
+            playAudio(statement.generatedAudio);
+        }
+    };
 
-const playAudio = async (audioBlob) => {
-    try {
-        const url = window.URL.createObjectURL(audioBlob);
-        const audio = new Audio(url);
-        audio.play();
-    } catch (error) {
-        console.error('Error in playing audio:', error);
-    }
-};
+    const playAudio = async (audioBlob) => {
+        try {
+            const url = window.URL.createObjectURL(audioBlob);
+            const audio = new Audio(url);
+            audio.play();
+        } catch (error) {
+            console.error('Error in playing audio:', error);
+        }
+    };
 
-  return (
-    <div className="flex flex-col items-center p-4">
-    <div className="w-full max-w-xl space-y-4">
-        {dialog.map((statement, index) => (
-            <div key={index} className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 bg-gray-100 p-3 rounded-lg">
-                <select
-                    className="flex-1 bg-white border border-gray-300 p-2 rounded"
-                    value={statement.speaker}
-                    onChange={(e) => updateStatement(index, 'speaker', e.target.value)}
-                >
-                    {speakers.map((speaker, i) => (
-                        <option key={i} value={speaker.voice_id}>{speaker.name}</option>
-                    ))}
-                </select>
-                <input
-                    className="flex-2 bg-white border border-gray-300 p-2 rounded"
-                    type="text"
-                    value={statement.text}
-                    onChange={(e) => updateStatement(index, 'text', e.target.value)}
-                    placeholder="Enter text here"
-                />
-                <div className="flex flex-row items-center space-y-0 space-x-2 bg-gray-100 p-3 rounded-lg">
-                    <button 
-                        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                        onClick={() => removeStatement(index)}>
-                        <FaTrash />
-                    </button>
-                    <button 
-                        className={`font-bold py-2 px-4 rounded ${statement.text === statement.prevText ? 'bg-green-500 hover:bg-green-700 text-white' : 'bg-yellow-500 hover:bg-yellow-700 text-white'}`}
-                        onClick={() => generateAudioForStatement(index)}
-                        title={statement.text === statement.prevText ? 'Regenerate Audio' : 'Generate Audio'}>
-                        {statement.text === statement.prevText ? <FaRedo /> : <FaLightbulb />}
-                    </button>
-                    <button 
-                        className={`font-bold py-2 px-4 rounded ${statement.generatedAudio && statement.text === statement.prevText ? 'bg-blue-500 hover:bg-blue-700 text-white' : 'bg-gray-500 text-gray-300 cursor-not-allowed'}`}
-                        onClick={() => playStatementAudio(statement)}
-                        disabled={!statement.generatedAudio || statement.text !== statement.prevText}
-                        title={statement.generatedAudio && statement.text === statement.prevText ? 'Play Audio' : 'Audio not available, generate it first with the other button.'}>
-                        <FaPlay />
+    var saveBlob = (function () {
+        var a = document.createElement("a");
+        document.body.appendChild(a);
+        a.style = "display: none";
+        return function (blob, fileName) {
+            var url = window.URL.createObjectURL(blob);
+            a.href = url;
+            a.download = fileName;
+            a.click();
+            window.URL.revokeObjectURL(url);
+        };
+    }());
+
+    return (
+        <div className="flex flex-col items-center p-4">
+            <div className="w-full max-w-xl space-y-4">
+                {dialog.map((statement, index) => (
+                    <div key={index} className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2 bg-gray-100 p-3 rounded-lg">
+                        <select
+                            className="flex-1 bg-white border border-gray-300 p-2 rounded"
+                            value={statement.speaker}
+                            onChange={(e) => updateStatement(index, 'speaker', e.target.value)}
+                        >
+                            {speakers.map((speaker, i) => (
+                                <option key={i} value={speaker.voice_id}>{speaker.name}</option>
+                            ))}
+                        </select>
+                        <input
+                            className="flex-2 bg-white border border-gray-300 p-2 rounded"
+                            type="text"
+                            value={statement.text}
+                            onChange={(e) => updateStatement(index, 'text', e.target.value)}
+                            placeholder="Enter text here"
+                        />
+                        <div className="flex flex-row items-center space-y-0 space-x-2 bg-gray-100 p-3 rounded-lg">
+                            <button
+                                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                                onClick={() => removeStatement(index)}>
+                                <FaTrash />
+                            </button>
+                            <button
+                                className={`font-bold py-2 px-4 rounded ${statement.text === statement.prevText ? 'bg-green-500 hover:bg-green-700 text-white' : 'bg-yellow-500 hover:bg-yellow-700 text-white'}`}
+                                onClick={() => generateAudioForStatement(index)}
+                                title={statement.text === statement.prevText ? 'Regenerate Audio' : 'Generate Audio'}>
+                                {statement.text === statement.prevText ? <FaRedo /> : <FaLightbulb />}
+                            </button>
+                            <button
+                                className={`font-bold py-2 px-4 rounded ${statement.generatedAudio && statement.text === statement.prevText ? 'bg-blue-500 hover:bg-blue-700 text-white' : 'bg-gray-500 text-gray-300 cursor-not-allowed'}`}
+                                onClick={() => playStatementAudio(statement)}
+                                disabled={!statement.generatedAudio || statement.text !== statement.prevText}
+                                title={statement.generatedAudio && statement.text === statement.prevText ? 'Play Audio' : 'Audio not available, generate it first with the other button.'}>
+                                <FaPlay />
+                            </button>
+                        </div>
+                    </div>
+                ))}
+                <div className="flex justify-center">
+                    <button
+                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        onClick={addStatement}>
+                        Add Statement
                     </button>
                 </div>
             </div>
-        ))}
-        <div className="flex justify-center">
-            <button 
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={addStatement}>
-                Add Statement
-            </button>
+            <div>
+                <div className="flex justify-center py-4">
+                    <h2>Whole debate audio</h2>
+                </div>
+                <div className="flex flex-col sm:flex-row space-y-2 sm:space-y-0 sm:space-x-2">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => generateDialogAudio()}>Generate audio dialog</button>
+                    <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${!generatedDialogAudio ? 'opacity-50 cursor-not-allowed' : ''}`} onClick={() => playAudio(generatedDialogAudio)} disabled={!generatedDialogAudio}>Play audio dialog</button>
+                    <button className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ${!generatedDialogAudio ? 'opacity-50 cursor-not-allowed' : ''}`} onClick={() => saveBlob(generatedDialogAudio, 'dialog.mp3')} disabled={!generatedDialogAudio}>Save audio dialog</button>
+                </div>
+            </div>
         </div>
-      </div>
-      <div>
-        <h2>Generate Audio</h2>
-        <button onClick={() => generateAudioForAllStatements()}>Generate Audio for all statements</button>
-        <button onClick={() => generateDialogAudio()}>Generate audio dialog</button>
-      </div>
-    </div>
-    
-  );
+
+    );
 };
 
 export default DialogCreation;
